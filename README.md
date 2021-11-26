ffc9f675e173e0ec8afdd9936cd4f41e55b754f1
moacs <moacs@msn.com>
2021-11-26 23:00:03 +0800

    fix

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# trojan
+东拼西凑的 Trojan 一键安装脚本
+
+curl -O https://raw.githubusercontent.com/moacs/trojan/master/trojan.sh && chmod  +x trojan.sh && ./trojan.sh
+
+wget --no-check-certificate https://raw.githubusercontent.com/moacs/trojan/master/trojan.sh && chmod +x trojan.sh && ./trojan.sh
---
+++ trojan.sh
@@ -0,0 +1,203 @@
+#!/bin/bash
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+release="ubuntu"
+systemPackage="apt-get"
+systempwd="/lib/systemd/system/"
+
+function install_trojan(){
+systemctl stop ufw
+systemctl disable ufw
+apt update
+$systemPackage -y install nginx certbot wget unzip zip curl tar  >/dev/null 2>&1
+systemctl enable nginx.service
+yellow "==========请输入域名============="
+read your_domain
+yellow "==========请输入密码============="
+read trojan_passwd
+
+real_addr=`ping ${your_domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
+local_addr=`curl ipv4.icanhazip.com`
+if [ $real_addr == $local_addr ] ; then
+	green "=========================================="
+	green "       域名解析正常，开始安装trojan"
+	green "=========================================="
+	sleep 1s
+cat > /etc/nginx/nginx.conf <<-EOF
+user  root;
+worker_processes  1;
+error_log  /var/log/nginx/error.log warn;
+pid        /var/run/nginx.pid;
+events {
+    worker_connections  1024;
+}
+http {
+    include       /etc/nginx/mime.types;
+    default_type  application/octet-stream;
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+    access_log  /var/log/nginx/access.log  main;
+    sendfile        on;
+    #tcp_nopush     on;
+    keepalive_timeout  120;
+    client_max_body_size 20m;
+    #gzip  on;
+    server {
+        listen       80;
+        server_name  $your_domain;
+        root /usr/share/nginx/html;
+        index index.php index.html index.htm;
+    }
+}
+EOF
+# /etc/letsencrypt/live/sight.moacs.com
+#   ssl_certificate /etc/letsencrypt/live/$your_domain/fullchain.pem;
+#     ssl_certificate_key /etc/letsencrypt/live/$your_domain/privkey.pem;
+	systemctl restart nginx.service
+	#申请https证书
+    certbot certonly --agree-tos --email moacs@msn.com --webroot -w /usr/share/nginx/html -d $your_domain
+	if test -s /etc/letsencrypt/live/$your_domain/fullchain.pem; then
+        cd /srv
+	wget https://github.com/trojan-gfw/trojan/releases/download/v1.14.0/trojan-1.14.0-linux-amd64.tar.xz
+	tar xf trojan-1.*
+	rm -rf /srv/trojan/config.json
+	cat > /srv/trojan/config.json <<-EOF
+{
+    "run_type": "server",
+    "local_addr": "0.0.0.0",
+    "local_port": 443,
+    "remote_addr": "127.0.0.1",
+    "remote_port": 80,
+    "password": [
+        "$trojan_passwd"
+    ],
+    "log_level": 1,
+    "ssl": {
+        "cert": "/usr/src/trojan-cert/fullchain.cer",
+        "key": "/usr/src/trojan-cert/private.key",
+        "key_password": "",
+        "cipher_tls13":"TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
+	"prefer_server_cipher": true,
+        "alpn": [
+            "http/1.1"
+        ],
+        "reuse_session": true,
+        "session_ticket": false,
+        "session_timeout": 600,
+        "plain_http_response": "",
+        "curves": "",
+        "dhparam": ""
+    },
+    "tcp": {
+        "no_delay": true,
+        "keep_alive": true,
+        "fast_open": false,
+        "fast_open_qlen": 20
+    },
+    "mysql": {
+        "enabled": false,
+        "server_addr": "127.0.0.1",
+        "server_port": 3306,
+        "database": "trojan",
+        "username": "trojan",
+        "password": ""
+    }
+}
+EOF
+
+cat > ${systempwd}trojan.service <<-EOF
+[Unit]  
+Description=trojan  
+After=network.target  
+   
+[Service]  
+Type=simple  
+PIDFile=/usr/src/trojan/trojan/trojan.pid
+ExecStart=/usr/src/trojan/trojan -c "/usr/src/trojan/config.json"  
+ExecReload=  
+ExecStop=/usr/src/trojan/trojan  
+PrivateTmp=true  
+   
+[Install]  
+WantedBy=multi-user.target
+EOF
+
+	chmod +x ${systempwd}trojan.service
+	systemctl start trojan.service
+	systemctl enable trojan.service
+	green "Trojan已安装完成"
+}
+
+function remove_trojan(){
+    red "================================"
+    red "即将卸载trojan"
+    red "同时卸载安装的nginx"
+    red "================================"
+    systemctl stop trojan
+    systemctl disable trojan
+    rm -f ${systempwd}trojan.service
+    if [ "$release" == "centos" ]; then
+        yum remove -y nginx
+    else
+        apt autoremove -y nginx
+    fi
+    rm -rf /usr/src/trojan*
+    rm -rf /usr/share/nginx/html/*
+    green "=============="
+    green "trojan删除完毕"
+    green "=============="
+}
+
+function bbr_boost_sh(){
+    bash <(curl -L -s -k "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh")
+}
+
+start_menu(){
+    clear
+    green " ===================================="
+    green " Trojan 一键安装自动脚本      "
+    green " ===================================="
+    echo
+    red " ===================================="
+    yellow " 1. 一键安装 Trojan"
+    red " ===================================="
+    yellow " 2. 安装 4 IN 1 BBRPLUS加速脚本"
+    red " ===================================="
+    yellow " 3. 一键卸载 Trojan"
+    red " ===================================="
+    yellow " 0. 退出脚本"
+    red " ===================================="
+    echo
+    read -p "请输入数字:" num
+    case "$num" in
+    1)
+    install_trojan
+    ;;
+    2)
+    bbr_boost_sh 
+    ;;
+    3)
+    remove_trojan
+    ;;
+    0)
+    exit 1
+    ;;
+    *)
+    clear
+    red "请输入正确数字"
+    sleep 1s
+    start_menu
+    ;;
+    esac
+}
+
+start_menu
