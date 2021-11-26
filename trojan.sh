f972aa2df817994bd083f16f5d56dba91cae53f8
moacs <moacs@msn.com>
2021-11-26 23:25:19 +0800

    format

--- trojan.sh
+++ trojan.sh
@@ -15,24 +15,24 @@ systemPackage="apt-get"
 systempwd="/lib/systemd/system/"
 
 install_trojan(){
-systemctl stop ufw
-systemctl disable ufw
-apt update
-$systemPackage -y install nginx certbot wget unzip zip curl tar  >/dev/null 2>&1
-systemctl enable nginx.service
-yellow "==========请输入域名============="
-read your_domain
-yellow "==========请输入密码============="
-read trojan_passwd
-
-real_addr=`ping ${your_domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
-local_addr=`curl ipv4.icanhazip.com`
-if [ $real_addr != $local_addr ] ; then
-	green "=========================================="
-	red "域名解析地址  $real_addr 与本地地址 $local_addr 不符"
-	green "=========================================="
-    exit 1
-fi
+    systemctl stop ufw
+    systemctl disable ufw
+    apt update
+    $systemPackage -y install nginx certbot wget unzip zip curl tar  >/dev/null 2>&1
+    systemctl enable nginx.service
+    yellow "==========请输入域名============="
+    read your_domain
+    yellow "==========请输入密码============="
+    read trojan_passwd
+    
+    real_addr=`ping ${your_domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
+    local_addr=`curl ipv4.icanhazip.com`
+    if [ $real_addr != $local_addr ] ; then
+        green "=========================================="
+        red "域名解析地址  $real_addr 与本地地址 $local_addr 不符"
+        green "=========================================="
+        exit 1
+    fi
 cat > /etc/nginx/nginx.conf <<-EOF
 user  root;
 worker_processes  1;
@@ -61,24 +61,24 @@ http {
     }
 }
 EOF
-	systemctl restart nginx.service
-	#申请https证书
+    systemctl restart nginx.service
+    #申请https证书
     certbot certonly --agree-tos --email moacs@msn.com --webroot -w /usr/share/nginx/html -d $your_domain
-	if test -s /etc/letsencrypt/live/$your_domain/fullchain.pem; then
-	green "=========================================="
-	yellow "证书申请成功"
-	green "=========================================="
+    if test -s /etc/letsencrypt/live/$your_domain/fullchain.pem; then
+        green "=========================================="
+        yellow "证书申请成功"
+        green "=========================================="
     else
-	green "=========================================="
-	red "证书申请失败"
-	green "=========================================="
-    exit 1
+        green "=========================================="
+        red "证书申请失败"
+        green "=========================================="
+        exit 1
     fi
-
+    
     cd /srv
-	wget https://github.com/trojan-gfw/trojan/releases/download/v1.14.0/trojan-1.14.0-linux-amd64.tar.xz
-	tar xf trojan-1.*
-	rm -rf /srv/trojan/config.json
+    wget https://github.com/trojan-gfw/trojan/releases/download/v1.14.0/trojan-1.14.0-linux-amd64.tar.xz
+    tar xf trojan-1.*
+    rm -rf /srv/trojan/config.json
 	cat > /srv/trojan/config.json <<-EOF
 {
     "run_type": "server",
@@ -95,7 +95,7 @@ EOF
         "key": "/etc/letsencrypt/live/$your_domain/privkey.pem",
         "key_password": "",
         "cipher_tls13":"TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
-	"prefer_server_cipher": true,
+	    "prefer_server_cipher": true,
         "alpn": [
             "http/1.1"
         ],
@@ -122,28 +122,28 @@ EOF
     }
 }
 EOF
-
+    
 cat > ${systempwd}trojan.service <<-EOF
-[Unit]  
-Description=trojan  
-After=network.target  
-   
-[Service]  
-Type=simple  
+[Unit]
+Description=trojan
+After=network.target
+
+[Service]
+Type=simple
 PIDFile=/usr/src/trojan/trojan/trojan.pid
-ExecStart=/usr/src/trojan/trojan -c "/usr/src/trojan/config.json"  
-ExecReload=  
-ExecStop=/usr/src/trojan/trojan  
-PrivateTmp=true  
-   
-[Install]  
+ExecStart=/usr/src/trojan/trojan -c "/usr/src/trojan/config.json"
+ExecReload=
+ExecStop=/usr/src/trojan/trojan
+PrivateTmp=true
+
+[Install]
 WantedBy=multi-user.target
 EOF
-
-	chmod +x ${systempwd}trojan.service
-	systemctl start trojan.service
-	systemctl enable trojan.service
-	green "Trojan已安装完成"
+    
+    chmod +x ${systempwd}trojan.service
+    systemctl start trojan.service
+    systemctl enable trojan.service
+    green "Trojan已安装完成"
 }
 
 remove_trojan(){
@@ -184,24 +184,24 @@ start_menu(){
     echo
     read -p "请输入数字:" num
     case "$num" in
-    1)
-    install_trojan
-    ;;
-    2)
-    bbr_boost_sh 
-    ;;
-    3)
-    remove_trojan
-    ;;
-    0)
-    exit 1
-    ;;
-    *)
-    clear
-    red "请输入正确数字"
-    sleep 1s
-    start_menu
-    ;;
+        1)
+            install_trojan
+        ;;
+        2)
+            bbr_boost_sh
+        ;;
+        3)
+            remove_trojan
+        ;;
+        0)
+            exit 1
+        ;;
+        *)
+            clear
+            red "请输入正确数字"
+            sleep 1s
+            start_menu
+        ;;
     esac
 }
 
